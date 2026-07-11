18ac6bb47f472ccfbde61a3138b39f5b9b58a6b6
Loki Astari <Loki.Astari@gmail.com>
2026-07-10 21:11:01 -0700

    Add Test 1,2 and 4
        Test 4 Works.
        Test 1,2 Fail (as expected).
    
        I need to add the ability to load a structure from an Array.

--- src/NisseMCP/test/JsonRCPProtocolTest.cpp
+++ src/NisseMCP/test/JsonRCPProtocolTest.cpp
@@ -20,7 +20,37 @@ ThorsAnvil_MakeTrait(SubtractParam, minuend, subtrahend);
 
 using namespace ThorsAnvil::Nisse::MCP;
 
-TEST(JsonRCPProtocolTest, RPC_CallWithNamedParameters)
+TEST(JsonRCPProtocolTest, RPC_CallWithPositionalParameters1)
+{
+    ThorsAnvil::Nisse::MCP::ServerConfig    config;
+    ThorsAnvil::Nisse::MCP::Local           local{config};
+
+    local.addExecutor<SubtractParam>("subtract", [](SubtractParam const& param){return JsonRPC::Response{param.minuend - param.subtrahend};});
+
+    std::istringstream   command{R"({"jsonrpc": "2.0", "method": "subtract", "params": [42, 23], "id": 1})"};
+    std::ostringstream   result;
+
+    local.run(command, result);
+
+    EXPECT_EQ(R"({"jsonrpc":"2.0","result":19,"id":1})", result.str());
+}
+
+TEST(JsonRCPProtocolTest, RPC_CallWithPositionalParameters2)
+{
+    ThorsAnvil::Nisse::MCP::ServerConfig    config;
+    ThorsAnvil::Nisse::MCP::Local           local{config};
+
+    local.addExecutor<SubtractParam>("subtract", [](SubtractParam const& param){return JsonRPC::Response{param.minuend - param.subtrahend};});
+
+    std::istringstream   command{R"({"jsonrpc": "2.0", "method": "subtract", "params": [23, 42], "id": 2})"};
+    std::ostringstream   result;
+
+    local.run(command, result);
+
+    EXPECT_EQ(R"({"jsonrpc":"2.0","result":-19,"id":2})", result.str());
+}
+
+TEST(JsonRCPProtocolTest, RPC_CallWithNamedParameters1)
 {
     ThorsAnvil::Nisse::MCP::ServerConfig    config;
     ThorsAnvil::Nisse::MCP::Local           local{config};
@@ -33,4 +63,19 @@ TEST(JsonRCPProtocolTest, RPC_CallWithNamedParameters)
     local.run(command, result);
 
     EXPECT_EQ(R"({"jsonrpc":"2.0","result":19,"id":3})", result.str());
+}
+
+TEST(JsonRCPProtocolTest, RPC_CallWithNamedParameters2)
+{
+    ThorsAnvil::Nisse::MCP::ServerConfig    config;
+    ThorsAnvil::Nisse::MCP::Local           local{config};
+
+    local.addExecutor<SubtractParam>("subtract", [](SubtractParam const& param){return JsonRPC::Response{param.minuend - param.subtrahend};});
+
+    std::istringstream   command{R"({"jsonrpc": "2.0", "method": "subtract", "params": {"minuend": 42, "subtrahend": 23}, "id": 4})"};
+    std::ostringstream   result;
+
+    local.run(command, result);
+
+    EXPECT_EQ(R"({"jsonrpc":"2.0","result":19,"id":4})", result.str());
 }
