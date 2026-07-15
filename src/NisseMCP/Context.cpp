3d29a0b407f6723591e29e5fe74d9a363c53c554
Loki Astari <Loki.Astari@gmail.com>
2026-07-15 09:41:29 -0700

    Tidy up the `Context` class.

--- src/NisseMCP/Context.cpp
+++ src/NisseMCP/Context.cpp
@@ -5,3 +5,51 @@ using namespace ThorsAnvil::Nisse::MCP;
 ThorsAnvil::Serialize::PrinterConfig    Context::outputConfig{ThorsAnvil::Serialize::OutputType::Stream};
 JsonRPC::OptRequestId                   Context::defaultId;
 
+Context::Context(std::istream& input, std::ostream& output, Protocol protocol)
+    : protocol(protocol)
+    , input(input)
+    , output(output)
+    , requestId(defaultId)
+    , errorState(false)
+    , count(0)
+    , stream(false)
+{}
+
+Context::~Context()
+{
+    // Close the output array.
+    if (stream && count > 0) {
+        output << "]";
+    }
+    if (errorState) {
+        input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+}
+
+void Context::stop()
+{
+    input.setstate(std::ios_base::failbit);
+}
+
+bool Context::good() const
+{
+    return input.good();
+}
+
+void Context::error(int code, std::string_view message, JsonRPC::OptRequestId const& id)
+{
+    errorState = true;
+    std::string_view sep = !stream ? "" : (count == 0) ? "[" : ",";
+    ++count;
+    output << sep << ThorsAnvil::Serialize::jsonExporter(JsonRPC::Response{code, message, id}, outputConfig);
+}
+
+void Context::setId(IdRef id)
+{
+    requestId = id;
+}
+
+void Context::serverSideStream()
+{
+    stream = true;
+}
--- src/NisseMCP/Context.h
+++ src/NisseMCP/Context.h
@@ -25,48 +25,15 @@ namespace ThorsAnvil::Nisse::MCP
         std::size_t     count;
         bool            stream;
         public:
-            Context(std::istream& input, std::ostream& output, Protocol protocol = Protocol::v2025_11_25)
-                : protocol(protocol)
-                , input(input)
-                , output(output)
-                , requestId(defaultId)
-                , errorState(false)
-                , count(0)
-                , stream(false)
-            {}
-            ~Context()
-            {
-                // Close the output array.
-                if (stream && count > 0) {
-                    output << "]";
-                }
-                if (errorState) {
-                    input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                }
-            }
-            void stop()
-            {
-                input.setstate(std::ios_base::failbit);
-            }
-            bool good() const
-            {
-                return input.good();
-            }
-            void error(int code, std::string_view message, JsonRPC::OptRequestId const& id)
-            {
-                errorState = true;
-                std::string_view sep = !stream ? "" : (count == 0) ? "[" : ",";
-                ++count;
-                output << sep << ThorsAnvil::Serialize::jsonExporter(JsonRPC::Response{code, message, id}, outputConfig);
-            }
-            void setId(IdRef id)
-            {
-                requestId = id;
-            }
-            void serverSideStream()
-            {
-                stream = true;
-            }
+            Context(std::istream& input, std::ostream& output, Protocol protocol = Protocol::v2025_11_25);
+            ~Context();
+
+            void stop();
+            bool good() const;
+            void error(int code, std::string_view message, JsonRPC::OptRequestId const& id);
+            void setId(IdRef id);
+            void serverSideStream();
+
             template<typename T>
             void  addItem(T const& value)
             {
